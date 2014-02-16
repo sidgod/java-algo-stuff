ec3fc00f72448a9a344417e7f2cd63c773c80e47
SidGod <siddharth.god@gmail.com>
2014-02-16 11:48:52 +0530

    Basic maven project

---
+++ src/main/java/in/co/sh00nya/algo/App.java
@@ -0,0 +1,13 @@
+package in.co.sh00nya.algo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
